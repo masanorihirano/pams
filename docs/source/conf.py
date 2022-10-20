f52a0dc4e6504a742351117278e46d00c4b5a4ba
Masanori HIRANO <masa.hirano.1996@gmail.com>
2022-10-20 19:04:57 +0900

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ extensions = [
 ]
 
 templates_path = ["_templates"]
-exclude_patterns = []
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
