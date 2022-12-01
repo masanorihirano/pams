14ba7aa81675055ce6b9324f593cb8275eb5b1c8
bonochof <ryos427@yahoo.co.jp>
2022-12-01 17:20:48 +0900

    documentation for HighFrequencyAgent class

--- pams/agents/high_frequency_agent.py
+++ pams/agents/high_frequency_agent.py
@@ -4,4 +4,9 @@ from .base import Agent
 
 
 class HighFrequencyAgent(Agent, ABC):
+    """High-frequency trading Agent class.
+
+    This class inherits from the :class:`pams.agents.Agent` class and ABC class.
+    """
+
     pass
