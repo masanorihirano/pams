f73d12179aa676c7e64155466a9536d549e29799
Masanori HIRANO <masa.hirano.1996@gmail.com>
2022-12-22 10:22:24 +0900

    bug fix in executed log returns to agents

--- pams/runners/sequential.py
+++ pams/runners/sequential.py
@@ -382,6 +382,9 @@ class SequentialRunner(Runner):
                     logs: List[ExecutionLog] = market._execution()
                     self.simulator._update_agents_for_execution(execution_logs=logs)
                     for execution_log in logs:
+                        agent = self.simulator.id2agent[execution_log.buy_agent_id]
+                        agent.executed_order(log=execution_log)
+                        agent = self.simulator.id2agent[execution_log.buy_agent_id]
                         agent.executed_order(log=execution_log)
                         self.simulator._trigger_event_after_execution(
                             execution_log=execution_log
