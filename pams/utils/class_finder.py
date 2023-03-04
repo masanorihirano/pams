05119511f03f963625768a137c70c9f40e187955
bonochof <ryos427@yahoo.co.jp>
2023-03-04 11:33:36 +0900

    documentation for class_finder

--- pams/utils/class_finder.py
+++ pams/utils/class_finder.py
@@ -4,6 +4,15 @@ from typing import Type
 
 
 def find_class(name: str, optional_class_list: Optional[List[Type]] = None) -> Type:
+    """find class.
+
+    Args:
+        name (str): class name.
+        optional_class_list (List[Type], Optional): optional class list.
+
+    Returns:
+        Type: class type.
+    """
     _1 = __import__("pams", globals(), locals())
     _2 = __import__("pams.agents", globals(), locals(), ["*"])
     _3 = __import__("pams.events", globals(), locals(), ["*"])
