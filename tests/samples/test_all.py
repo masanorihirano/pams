70e1eed2af325e37d9c6181942ab60a5047a059b
Masanori HIRANO <masa.hirano.1996@gmail.com>
2023-04-26 17:45:10 +0900

    fix test

--- tests/samples/test_all.py
+++ tests/samples/test_all.py
@@ -31,7 +31,7 @@ def test_all() -> None:
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             env=env,
-            shell=False,
+            shell=not os.environ["OS"].startswith("Windows"),
         )
         if run.returncode != 0 or len(run.stdout) == 0:
             raise RuntimeError(
