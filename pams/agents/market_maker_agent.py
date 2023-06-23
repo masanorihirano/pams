cca9808cb25a41fcfb72130456b7c2f7a7972109
bonochof <ryos427@yahoo.co.jp>
2023-06-24 00:34:39 +0900

    format

--- pams/agents/market_maker_agent.py
+++ pams/agents/market_maker_agent.py
@@ -6,7 +6,8 @@ from typing import Union
 
 from pams.agents.high_frequency_agent import HighFrequencyAgent
 from pams.market import Market
-from pams.order import LIMIT_ORDER, Cancel
+from pams.order import LIMIT_ORDER
+from pams.order import Cancel
 from pams.order import Order
 from pams.utils.json_random import JsonRandom
 
@@ -45,8 +46,14 @@ class MarketMakerAgent(HighFrequencyAgent):
         if "netInterestSpread" not in settings:
             raise ValueError("netInterestSpread is required for MarketMakerAgent.")
         json_random: JsonRandom = JsonRandom(prng=self.prng)
-        self.net_interest_spread = json_random.random(json_value=settings["netInterestSpread"])
-        self.order_time_length = json_random.random(json_value=settings["orderTimeLength"] if "orderTimeLength" in settings else "2")
+        self.net_interest_spread = json_random.random(
+            json_value=settings["netInterestSpread"]
+        )
+        self.order_time_length = json_random.random(
+            json_value=settings["orderTimeLength"]
+            if "orderTimeLength" in settings
+            else "2"
+        )
         super().setup(settings=settings, accessible_markets_ids=accessible_markets_ids)
 
     def submit_orders(self, markets: List[Market]) -> List[Union[Order, Cancel]]:
@@ -56,10 +63,12 @@ class MarketMakerAgent(HighFrequencyAgent):
             - :func:`pams.agents.Agent.submit_orders`
         """
         base_price: float = self.get_base_price(markets)
-        if base_price != float('inf'):
+        if base_price != float("inf"):
             base_price = self.target_market.get_market_price()
         orders: List[Order] = []
-        price_margin: float = self.target_market.get_fundamental_price() * self.net_interest_spread * 0.5
+        price_margin: float = (
+            self.target_market.get_fundamental_price() * self.net_interest_spread * 0.5
+        )
         order_volume: int = 1
         orders.append(
             Order(
@@ -86,12 +95,18 @@ class MarketMakerAgent(HighFrequencyAgent):
         return orders
 
     def get_base_price(self, markets: List[Market]) -> float:
-        max_buy: float = -float('inf')
+        max_buy: float = -float("inf")
         for market in markets:
-            if self.is_market_accessible(market.market_id) and market.get_best_buy_price() is float:
+            if (
+                self.is_market_accessible(market.market_id)
+                and market.get_best_buy_price() is float
+            ):
                 max_buy = max(max_buy, market.get_best_buy_price())
-        min_sell: float = float('inf')
+        min_sell: float = float("inf")
         for market in markets:
-            if self.is_market_accessible(market.market_id) and market.get_best_sell_price() is float:
+            if (
+                self.is_market_accessible(market.market_id)
+                and market.get_best_sell_price() is float
+            ):
                 min_sell = min(min_sell, market.get_best_sell_price())
         return (max_buy + min_sell) / 2.0
