e4f3185e92efc549d1b7674860989fd17b912e72
bonochof <ryos427@yahoo.co.jp>
2023-06-23 18:39:37 +0900

    for test

--- pams/agents/__init__.py
+++ pams/agents/__init__.py
@@ -2,4 +2,5 @@ from .arbitrage_agent import ArbitrageAgent
 from .base import Agent
 from .fcn_agent import FCNAgent
 from .high_frequency_agent import HighFrequencyAgent
+from .market_share_fcn_agent import MarketShareFCNAgent
 from .test_agent import TestAgent
