d4e251f72b55a02198eac54094ec8adb80e7d2ea
bonochof <ryos427@yahoo.co.jp>
2023-06-24 00:33:47 +0900

    add MMAgent dynamics

--- pams/agents/market_maker_agent.py
+++ pams/agents/market_maker_agent.py
@@ -6,7 +6,7 @@ from typing import Union
 
 from pams.agents.high_frequency_agent import HighFrequencyAgent
 from pams.market import Market
-from pams.order import Cancel
+from pams.order import LIMIT_ORDER, Cancel
 from pams.order import Order
 from pams.utils.json_random import JsonRandom
 
@@ -50,31 +50,48 @@ class MarketMakerAgent(HighFrequencyAgent):
         super().setup(settings=settings, accessible_markets_ids=accessible_markets_ids)
 
     def submit_orders(self, markets: List[Market]) -> List[Union[Order, Cancel]]:
-        """submit orders based on FCN-based calculation.
+        """submit orders.
 
-        Create limit order to litMarket first, and then change the order destination to DarkPoolMarket with probability 1-d.
         .. seealso::
             - :func:`pams.agents.Agent.submit_orders`
         """
-        filter_markets: List[Market] = self.filter_markets(markets)
-        weights: List[float] = []
-        for market in filter_markets:
-            weights.append(float(self.get_sum_trade_volume(market)))
-        if len(filter_markets) == 0:
-            raise RuntimeError("filter_markets in MarketShareFCNAgent is empty.")
-        return self.submit_orders_by_market(random.choice(filter_markets))
+        base_price: float = self.get_base_price(markets)
+        if base_price != float('inf'):
+            base_price = self.target_market.get_market_price()
+        orders: List[Order] = []
+        price_margin: float = self.target_market.get_fundamental_price() * self.net_interest_spread * 0.5
+        order_volume: int = 1
+        orders.append(
+            Order(
+                agent_id=self.agent_id,
+                market_id=self.target_market.market_id,
+                is_buy=True,
+                kind=LIMIT_ORDER,
+                volume=order_volume,
+                price=base_price - price_margin,
+                ttl=self.order_time_length,
+            )
+        )
+        orders.append(
+            Order(
+                agent_id=self.agent_id,
+                market_id=self.target_market.market_id,
+                is_buy=False,
+                kind=LIMIT_ORDER,
+                volume=order_volume,
+                price=base_price + price_margin,
+                ttl=self.order_time_length,
+            )
+        )
+        return orders
 
-    def filter_markets(self, markets: List[Market]) -> List[Market]:
-        a: List[Market] = []
+    def get_base_price(self, markets: List[Market]) -> float:
+        max_buy: float = -float('inf')
         for market in markets:
-            if self.is_market_accessible(market_id=market.market_id):
-                a.append(market)
-        return a
-
-    def get_sum_trade_volume(self, market: Market) -> int:
-        t: int = market.get_time()
-        time_window_size: int = min(t, self.time_window_size)
-        volume: int = 0
-        for d in range(1, time_window_size + 1):
-            volume += market.get_executed_volume(t - d)
-        return volume
+            if self.is_market_accessible(market.market_id) and market.get_best_buy_price() is float:
+                max_buy = max(max_buy, market.get_best_buy_price())
+        min_sell: float = float('inf')
+        for market in markets:
+            if self.is_market_accessible(market.market_id) and market.get_best_sell_price() is float:
+                min_sell = min(min_sell, market.get_best_sell_price())
+        return (max_buy + min_sell) / 2.0
