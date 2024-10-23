f7ed1c7b2a952bde0a92e33d8992a83b5b25fac4
Masanori HIRANO <masa.hirano.1996@gmail.com>
2024-10-23 16:15:11 -0400

    fix typing

--- tests/pams/runners/delay_agent.py
+++ tests/pams/runners/delay_agent.py
@@ -1,5 +1,5 @@
 import time
-from typing import List
+from typing import List, Union
 
 from pams.agents.fcn_agent import FCNAgent
 from pams.market import Market
@@ -10,6 +10,6 @@ wait_time = 0.2  # seconds
 
 
 class FCNDelayAgent(FCNAgent):
-    def submit_orders(self, markets: List[Market]) -> List[Order | Cancel]:
+    def submit_orders(self, markets: List[Market]) -> List[Union[Order, Cancel]]:
         time.sleep(wait_time)  # Simulate a delay
         return super().submit_orders(markets)
