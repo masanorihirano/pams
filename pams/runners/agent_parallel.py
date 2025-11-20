89411c184c119c69326adc10ebeae3530d956237
Masanori Hirano <masa.hirano.1996@gmail.com>
2025-11-20 09:46:52 +0900

    add

--- pams/runners/agent_parallel.py
+++ pams/runners/agent_parallel.py
@@ -101,7 +101,7 @@ class MultiProcessAgentParallelRuner(MultiThreadAgentParallelRuner):
     In this runner, only normal agents are parallelized in each steps.
     This means that the number of agents that can be parallelized is limited by MAX_NORMAL_ORDERS.
 
-    Note: When you are using MultiProcessAgentParallelRuner, the definition of self-defined classes are saved to a diffenrent fils.
+    Note: When you are using MultiProcessAgentParallelRuner, the definition of self-defined classes are saved to a diffenrent file.
           This is because of the limitation of python multiprocessing.
     """
 
